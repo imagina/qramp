f3c42476a0b140be7c0f92e666e1c8b487ab0455
Angel Barrios <ae.barrios@hotmail.com>
2022-06-09 14:25:30 -0400

    +update: search component

---
+++ _components/searchLocal/index.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <q-input borderless standout dense rounded style="max-width: 100%"
+    color="primary" :placeholder="`${$tr('ifly.cms.label.search',{capitalize : true})}...`"
+    class="q-my-md q-ml-md q-mr-xs search" @input="$emit('value', value)" v-model="value">
+    <template v-slot:prepend>
+      <q-icon color="primary" class="q-pl-sm" name="search" />
+    </template>
+  </q-input>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      value: null
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .search
+      background #e9edf6
+      border-radius 99px
+</style>
