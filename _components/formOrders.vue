f4d1b4eccf24cf714075d792f635dccd2ce81c37
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-11-03 15:17:59 -0500

    add preNumber

--- _components/formOrders.vue
+++ _components/formOrders.vue
@@ -275,7 +275,7 @@ export default {
         this.flight.responsibleId = updateData.data['responsibleId'] ? updateData.data['responsibleId'] : null;
         this.flight.faFlightId = updateData.data['faFlightId'] ? updateData.data['faFlightId'] : null;
         this.flight.flightStatusId = updateData.data['flightStatusId'] ? updateData.data['flightStatusId'] : null;
-
+        this.fligth.preFlightNumber = updateData.data['preFlightNumber'] ? updateData.data['preFlightNumber'] : null;
         qRampStore().setResponsible(updateData.data['responsible']);
         this.flight.date = updateData.data['date'] ? updateData.data['date'].toString() : ''
         const customerName = updateData.data.customer ? updateData.data.customer.customerName : null;
