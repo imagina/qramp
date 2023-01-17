89047db22829058a4fddfc3bdff5ec673ed760c4
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2023-01-17 13:08:21 -0500

    se agrega lineForm comentarios

--- _components/schedule/lineForm.vue
+++ _components/schedule/lineForm.vue
@@ -1,56 +1,65 @@
 <template>
-  <q-form ref="lineForm">
-    <div
-      class="
-        tw-border
-        tw-rounded-lg
-        tw-py-3
-        tw-px-4
-        tw-mx-2
-        tw-my-1
-        lg:tw-flex
-        lg:tw-space-x-2
-        lg:tw-flex-wrap
-      "
-    >
-      <div v-for="(field, keyField) in fields.form" :key="keyField">
-        <dynamic-field
-          :field="field"
-          v-model="event[keyField]"
-          class="tw-w-full lg:tw-w-48"
-          :class="{ 'tw-hidden tw-w-0': keyField === 'stationId' }"
-        />
+  <div>
+    <q-form ref="lineForm">
+      <div
+        class="
+          tw-border tw-rounded-lg tw-py-3 tw-px-4 tw-mx-2 tw-my-1
+          lg:tw-flex lg:tw-space-x-2 lg:tw-flex-wrap
+        "
+      >
+        <div v-for="(field, keyField) in fields.form" :key="keyField">
+          <dynamic-field
+            :field="field"
+            v-model="event[keyField]"
+            class="tw-w-full lg:tw-w-48"
+            :class="{ 'tw-hidden tw-w-0': keyField === 'stationId' }"
+          />
+        </div>
+        <div class="tw-space-x-2 tw-my-1">
+          <button
+            v-if="event.isUpdate && !isBlank"
+            class="tw-bg-blue-800 tw-rounded-lg tw-px-2 tw-py-1 tw-text-white"
+            @click.prevent="save"
+          >
+            <i class="fa-light fa-floppy-disk" />
+            <q-tooltip>
+              {{ $tr("isite.cms.label.save") }}
+            </q-tooltip>
+          </button>
+          <button
+            v-if="event.isUpdate && !isBlank"
+            class="tw-bg-blue-800 tw-rounded-lg tw-px-2 tw-py-1 tw-text-white"
+            @click.prevent="openCommentsModal"
+          >
+            <i class="fa-light fa-comment" />
+            <q-tooltip>
+              {{ $tr("isite.cms.label.comment") }}
+            </q-tooltip>
+          </button>
+          <button
+            v-if="event.isUpdate"
+            @click.prevent="$emit('dismissEvent', event)"
+            class="tw-bg-blue-800 tw-rounded-lg tw-px-2 tw-py-1 tw-text-white"
+          >
+            <i class="fa-light fa-xmark"></i>
+            <q-tooltip> Discard </q-tooltip>
+          </button>
+        </div>
       </div>
-      <div class="tw-space-x-2 tw-my-1">
-        <button
-          v-if="event.isUpdate && !isBlank"
-          class="tw-bg-blue-800 tw-rounded-lg tw-px-2 tw-py-1 tw-text-white"
-          @click.prevent="save"
-        >
-          <i class="fa-light fa-floppy-disk" />
-          <q-tooltip>
-            {{ $tr('isite.cms.label.save') }}
-          </q-tooltip>
-        </button>
-        <button
-          v-if="event.isUpdate"
-          @click.prevent="$emit('dismissEvent', event)"
-          class="tw-bg-blue-800 tw-rounded-lg tw-px-2 tw-py-1 tw-text-white"
-        >
-          <i class="fa-light fa-xmark"></i>
-          <q-tooltip>
-            Discard
-          </q-tooltip>
-        </button>
-      </div>
-    </div>
-  </q-form>
+    </q-form>
+    <commentsModal
+      v-if="event.id" 
+      ref="commentsModal"
+      :commentableId="event.id"
+    />
+  </div>
 </template>
 <script>
 import scheduleField from "./fields/scheduleField.js";
-import qRampStore from '../../_store/qRampStore.js';
-
+import qRampStore from "../../_store/qRampStore.js";
+import commentsModal from "./modals/commentsModal.vue";
 export default {
+  components: { commentsModal },
   mixins: [scheduleField],
   props: {
     event: {
@@ -69,19 +78,21 @@ export default {
   },
   computed: {
     tranformaDate() {
-        const inboundScheduledArrival = `${this.$moment(this.event.inboundScheduledArrival).format('MM/DD/YYYY')} ${this.event.sta}`;
-        this.event.sta = this.$moment(this.event.sta, 'HH:mm:ss').format('HH:mm');
-        this.event.std = this.$moment(this.event.std, 'HH:mm:ss').format('HH:mm');
-        return {
-            sta: this.event.sta,
-            std: this.event.std,
-            stationId: this.event.stationId,
-            preFlightNumber: this.event.preFlightNumber,
-            gateId: this.event.gateId,
-            flightStatusId: this.event.flightStatusId,
-            aircraftTypeId: this.event.aircraftTypeId,
-            inboundScheduledArrival: inboundScheduledArrival,
-        }
+      const inboundScheduledArrival = `${this.$moment(
+        this.event.inboundScheduledArrival
+      ).format("MM/DD/YYYY")} ${this.event.sta}`;
+      this.event.sta = this.$moment(this.event.sta, "HH:mm:ss").format("HH:mm");
+      this.event.std = this.$moment(this.event.std, "HH:mm:ss").format("HH:mm");
+      return {
+        sta: this.event.sta,
+        std: this.event.std,
+        stationId: this.event.stationId,
+        preFlightNumber: this.event.preFlightNumber,
+        gateId: this.event.gateId,
+        flightStatusId: this.event.flightStatusId,
+        aircraftTypeId: this.event.aircraftTypeId,
+        inboundScheduledArrival: inboundScheduledArrival,
+      };
     },
     isBlank() {
       return qRampStore().getIsblank();
@@ -93,11 +104,14 @@ export default {
         this.$refs.lineForm.validate().then(async (success) => {
           if (success) {
             if (this.event.isUpdate && typeof this.event.id === "string") {
-              this.event.isUpdate = false;  
+              this.event.isUpdate = false;
               this.$emit("addSchedule", this.tranformaDate);
             } else {
               this.event.isUpdate = false;
-              this.$emit("updateSchedule", {...this.tranformaDate, id: this.event.id});
+              this.$emit("updateSchedule", {
+                ...this.tranformaDate,
+                id: this.event.id,
+              });
             }
           }
         });
@@ -105,6 +119,11 @@ export default {
         console.log(error);
       }
     },
+    openCommentsModal() {
+      if(this.$refs.commentsModal){
+        this.$refs.commentsModal.showModal();
+      }
+    }
   },
 };
 </script>
---
+++ _components/schedule/modals/commentsModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <master-modal
+    v-model="visible"
+    title="Comments"
+    :persistent="true"
+    :loading="loading"
+    :actions="actions"
+    @hide="hideModal"
+    width="60%"
+    :maximized="$q.screen.lt.md"
+  >
+    <comments
+      v-if="commentableId"
+      apiRoute="apiRoutes.qramp.comments"
+      :commentableId="Number(commentableId)"
+      commentableType="Modules\Ramp\Entities\WorkOrder"
+      class="tw-py-4"
+    />
+  </master-modal>
+</template>
+
+<script>
+import comments from "@imagina/qsite/_components/master/comments/index.vue";
+
+export default {
+  components: { comments },
+  props: {
+    commentableId: {
+      type: Number,
+      default: () => 0,
+    },
+  },
+  data: () => ({
+    visible: false,
+    loading: false,
+    stationId: null,
+  }),
+  computed: {},
+  methods: {
+    hideModal() {
+      this.visible = false;
+    },
+    showModal() {
+      this.visible = true;
+    },
+  },
+};
+</script>
