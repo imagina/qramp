50253e970dcfbc41ef55455a0e60362681676207
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2023-01-18 11:44:23 -0500

    se cambia la ruta del commentarios

--- _config/apiRoutes.js
+++ _config/apiRoutes.js
@@ -5,9 +5,6 @@ const urlBase = `/${moduleName}/${moduleVersion}`
 const setupModuleName = 'setup';
 const setupUrlBase = `/${setupModuleName}/${moduleVersion}`
 
-const moduleComments = 'icomments';
-const commentsUrlBase = `/${moduleComments}/${moduleVersion}`
-
 export default {
   urlBase: urlBase,
   version: moduleVersion,
@@ -33,5 +30,5 @@ export default {
   workOrdersByStatus: `${urlBase}/dashboard/work-orders-by-status`,
   percentageWorkOrdersPosted: `${urlBase}/dashboard/percentage-work-orders-posted`,
   workOrdersByStatusLines: `${urlBase}/dashboard/work-orders-by-status-lines`,
-  comments: `${commentsUrlBase}/comments`,
+  comments: `${urlBase}/comments`,
 }
