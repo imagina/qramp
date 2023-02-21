2f58ea7931eeeac1b2aaafa28f3ea5faf5eb513f
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2023-02-21 17:41:32 -0500

    se documenta el codigo

--- _components/cargo/store/cargo.ts
+++ _components/cargo/store/cargo.ts
@@ -6,12 +6,14 @@ import {
     PayloadContract,
     UseCargoStoreContract
 } from '../@Contract/cargo.contract';
+/* A model for the delayList array. */
 const modelDelay = [
     {
       code: '',
       hours: ''
     },
 ];
+/* Creating a reactive object. */
 const state = reactive<CargoStoreContract>({
     form: {
         inboundCargoTotalUldsUnloaded: '',
@@ -24,10 +26,23 @@ const state = reactive<CargoStoreContract>({
     delayList: [...modelDelay],
     delay: false,
 });
+/**
+ * It returns an object with a bunch of functions that are used to get and set the state of the store.
+ * @returns {UseCargoStoreContract} A function that returns an object.
+ */
 export default function cargoStore(): UseCargoStoreContract {
+    /**
+     * &gt; It returns the form property of the state object.
+     * @returns {FormContract} The form object.
+     */
     function getForm(): FormContract {
         return state.form;
     }
+    /**
+     * It takes a FormContract object and sets the state.form object to the values of the FormContract
+     * object.
+     * @param {FormContract} cargo - FormContract
+     */
     function setForm(cargo: FormContract): void {
         state.form.inboundCargoTotalUldsUnloaded = cargo['inboundCargoTotalUldsUnloaded'] ? cargo['inboundCargoTotalUldsUnloaded'].toString() : '';
         state.form.inboundCargoBulkUnloaded = cargo['inboundCargoBulkUnloaded'] ? cargo['inboundCargoBulkUnloaded'].toString() : '';
@@ -36,9 +51,19 @@ export default function cargoStore(): UseCargoStoreContract {
         state.form.cargoTotalKilosLoaded = cargo['cargoTotalKilosLoaded'] ? cargo['cargoTotalKilosLoaded'].toString() : '';
         state.form.cargoTotalKilosUnloaded = cargo['cargoTotalKilosUnloaded'] ? cargo['cargoTotalKilosUnloaded'].toString() : '';
     }
+    /**
+     * This function returns the delayList property of the state object.
+     * @returns {DelayListContract} The delayList array.
+     */
     function getDelayList(): DelayListContract[] {
         return state.delayList;
     }
+    /**
+     * If the delay array is empty, set the delayList to the modelDelay array, otherwise set the
+     * delayList to the delay array.
+     * @param {DelayListContract[]} data - DelayListContract[]
+     * @returns the state.delayList.
+     */
     function setDelayList(data: DelayListContract[]): void {
         const delay = data['delay'] || [];
         if( delay.length === 0) {
@@ -51,12 +76,25 @@ export default function cargoStore(): UseCargoStoreContract {
             ?  delay 
             : [...modelDelay];
     }
+    /**
+     * The function `getDelay` returns a boolean value.
+     * @returns {boolean} The value of the delay property of the state object.
+    */
     function getDelay(): boolean {
         return state.delay;
     }
+    /**
+     * This function takes a boolean value and sets the delay property of the state object to that
+     * value.
+     * @param {boolean} value - The value to set the delay to.
+     */
     function setDelay(value: boolean): void {
         state.delay = value;
     }
+    /**
+     * It returns an object with two properties, one of which is an array of objects.
+     * @returns {PayloadContract} The payload function is returning an object with two properties.
+     */
     function payload(): PayloadContract {
         const delay = state.delay ? state.delayList.filter(items => {
             return items.code && items.hours
@@ -66,6 +104,9 @@ export default function cargoStore(): UseCargoStoreContract {
             cargo: state.form,
         }
     }
+    /**
+     * It resets the state of the form and delayList to their default values.
+     */
     function reset(): void {
         state.form = {
             inboundCargoTotalUldsUnloaded: '',
