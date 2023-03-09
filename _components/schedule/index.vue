bbf81bb739b786f190558d7d23c8f7ebea52e129
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2023-03-09 16:05:01 -0500

    Se agrega componente badComent

--- _components/schedule/index.vue
+++ _components/schedule/index.vue
@@ -73,79 +73,11 @@
               :class="classSchedule(event)"
               @click.stop.prevent="editSchedule(event)"
             >
-              <div v-if="event.comments > 0 && permisionComments" >
-              <div 
-                class="
-                  tw-relative 
-                  tw-inline-flex 
-                  tw-items-center
-                  tw-text-sm 
-                  tw-font-medium 
-                  tw-text-center 
-                  tw-rounded-lg 
-                  hover:bg-blue-800 
-                  focus:ring-4 
-                  focus:outline-none 
-                  focus:ring-blue-300 
-                  dark:bg-blue-600 
-                  dark:hover:bg-blue-700 
-                  dark:focus:ring-blue-800
-                  tw-mx-1
-                  tw-cursor-pointer"
-                >
-                <i
-                  class="
-                    fa-light 
-                    fa-comment-lines 
-                    tw-pr-1 
-                    tw-text-red-500 
-                    tw-font-semibold" 
-                />
-                <div
-                  v-if="event.comments && event.comments > 0"
-                  class="
-                  tw-absolute 
-                  tw-inline-flex 
-                  tw-items-center tw-justify-center 
-                  tw-w-4 
-                  tw-h-4
-                  tw-font-bold 
-                  tw-text-white
-                  tw-border 
-                  tw-bg-red-500 
-                  tw-rounded-full 
-                  tw--top-1 
-                  tw--right-2 dark:border-gray-900"
-                  style="font-size: 7px;"
-                >{{  event.comments  }}</div>
-                <q-tooltip
-                    v-model="event.showCommentTooltip"
-                    :content-class="{
-                      'tooltipComments': true,
-                      'tw-text-center': loadingComment,
-                    }"
-                    @input="changeLastComment(event)" 
-                    :offset="[10, 10]">
-                      <div v-if="loadingComment" class="tw-py-2">
-                        <i 
-                          class="
-                           fa-thin 
-                           fa-spinner-third 
-                           fa-spin 
-                           fa-pulse 
-                           tw-text-2xl"
-                        />
-                      </div>
-                      <div v-else>
-                          <p class="tw-text-sm tw-font-semibold">Last comment</p>
-                          <div
-                            class="tw-text-sm"
-                            v-html="lastComment"
-                          />
-                      </div>
-                </q-tooltip>
-              </div>
-              </div>
+              <badgeComment 
+                :event="event" 
+                :sizeBadge="7"
+                iconClass="tw-text-sm"
+              />
               <i
                 class="
                  fa-solid 
@@ -190,78 +122,9 @@
                 class="tw-font-semibold"
                 :class="{'tw-w-1/2': event.id && scheduleType === 'day-agenda'}"
               >
-                <div
-                  v-if="event.comments > 0 && permisionComments" 
-                  class="
-                    tw-relative 
-                    tw-inline-flex 
-                    tw-items-center
-                    tw-font-medium 
-                    tw-text-center 
-                    tw-rounded-lg 
-                    hover:bg-blue-800 
-                    focus:ring-4 
-                    focus:outline-none 
-                    focus:ring-blue-300 
-                    dark:bg-blue-600 
-                    dark:hover:bg-blue-700 
-                    dark:focus:ring-blue-800
-                    tw-text-xl
-                    tw-mx-2
-                    tw-cursor-pointer"
-                  >
-                  <i
-                    class="
-                      fa-light 
-                      fa-comment-lines 
-                      tw-pr-1 
-                      tw-text-red-500 
-                      tw-font-semibold" 
-                  />
-                  <div
-                    v-if="event.comments && event.comments > 0"
-                    class="
-                    tw-absolute 
-                    tw-inline-flex 
-                    tw-items-center tw-justify-center 
-                    tw-w-4 
-                    tw-h-4
-                    tw-font-bold 
-                    tw-text-white
-                    tw-border 
-                    tw-bg-red-500 
-                    tw-rounded-full 
-                    tw--top-1 
-                    tw--right-2 dark:border-gray-900"
-                    style="font-size: 9px;"
-                  >{{  event.comments  }}</div>
-                  <q-tooltip
-                      v-model="event.showCommentTooltip"
-                      :content-class="{
-                        'tooltipComments': true,
-                        'tw-text-center': loadingComment,
-                      }"
-                      @input="changeLastComment(event)" 
-                      :offset="[10, 10]">
-                        <div v-if="loadingComment" class="tw-py-2">
-                          <i 
-                            class="
-                            fa-thin 
-                            fa-spinner-third 
-                            fa-spin 
-                            fa-pulse 
-                            tw-text-2xl"
-                          />
-                        </div>
-                        <div v-else>
-                          <p class="tw-text-sm tw-font-semibold">Last comment</p>
-                          <div
-                            class="tw-text-sm"
-                            v-html="lastComment"
-                          />
-                        </div>
-                  </q-tooltip>
-                </div>
+                <badgeComment 
+                  :event="event" 
+                />
                 <i
                   class="
                    fa-solid 
@@ -422,6 +285,7 @@ import {
   getLastComment, 
   setLastComment, 
   getLoading } from '../../_store/actions/comments.ts';
+import badgeComment from './badgeComment.vue';
 export default {
   props:{
     isBlank: {
@@ -435,6 +299,7 @@ export default {
     formOrders,
     stationModal,
     lineForm,
+    badgeComment,
   },
   data() {
     return {
