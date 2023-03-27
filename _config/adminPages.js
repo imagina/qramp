cd0d30767f42161e6dfa711129260815cfa86506
José Alejandro Sabogal Loaiza <jose.alejandro.sabogal.loaiza@gmail.com>
2023-03-27 13:12:44 -0500

    change paths

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -5,7 +5,7 @@ export default {
     authenticated: true,
     path: '/ramp/work-orders/index',
     name: 'qramp.admin.workOrders',
-    crud: import('@imagina/qramp/_crud/workOrders'),
+    crud: import('../_crud/workOrders'),
     page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
     layout: () => import('@imagina/qsite/_layouts/master.vue'),
     title: 'ifly.cms.sidebar.workOrders',
