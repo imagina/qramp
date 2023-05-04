a548a6305d73b6c9cdb6050fbd8a31384ae2709a
Wilmerace <wilmerc@imaginacolombia.com>
2023-05-04 11:57:56 -0500

    Model

--- _components/schedule/models/modelHoursFilter.js
+++ _components/schedule/models/modelHoursFilter.js
@@ -1,34 +1,34 @@
 export default [
     {
-        label: '00 - 03 H',
-        value: '0-3'
+        label: '00 - 02:59 H',
+        value: '0-2'
     },
     {
-        label: '03 - 06 H',
+        label: '03:00 - 06:59 H',
         value: '3-6'
     },
     {
-        label: '06 - 09 H',
-        value: '6-9'
+        label: '07 - 09:59 H',
+        value: '7-9'
     },
     {
-        label: '09 - 12 H',
-        value: '9-12'
+        label: '10 - 12:59 H',
+        value: '10-12'
     },
     {
-        label: '12 - 15 H',
-        value: '12-15'
+        label: '13 - 15:59 H',
+        value: '13-15'
     },
     {
-        label: '15 - 18 H',
-        value: '15-18'
+        label: '16 - 18:59 H',
+        value: '16-18'
     },
     {
-        label: '18 - 21 H',
-        value: '18-21'
+        label: '19 - 21:59 H',
+        value: '19-21'
     },
     {
-        label: '21 - 23 H',
-        value: '21-23'
-    }
+        label: '22 - 23:59 H',
+        value: '22-23'
+    },
 ]
