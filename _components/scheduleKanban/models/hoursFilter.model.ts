1fb0fb74f0d98c6e68019a82825a300aab7fd018
Wilmerace <wilmerc@imaginacolombia.com>
2023-08-31 12:33:57 -0500

    Se modifica el label de horas

--- _components/scheduleKanban/models/hoursFilter.model.ts
+++ _components/scheduleKanban/models/hoursFilter.model.ts
@@ -5,35 +5,35 @@ const hoursFilter: HoursFilter[] = [
         value: '0-23'
     },
     {
-        label: '00 - 02:59 H',
+        label: '00:00 - 02:59',
         value: '0-2'
     },
     {
-        label: '03:00 - 06:59 H',
+        label: '03:00 - 06:59',
         value: '3-6'
     },
     {
-        label: '07 - 09:59 H',
+        label: '07:00 - 09:59',
         value: '7-9'
     },
     {
-        label: '10 - 12:59 H',
+        label: '10:00 - 12:59',
         value: '10-12'
     },
     {
-        label: '13 - 15:59 H',
+        label: '13:00 - 15:59',
         value: '13-15'
     },
     {
-        label: '16 - 18:59 H',
+        label: '16:00 - 18:59',
         value: '16-18'
     },
     {
-        label: '19 - 21:59 H',
+        label: '19:00 - 21:59',
         value: '19-21'
     },
     {
-        label: '22 - 23:59 H',
+        label: '22:00 - 23:59',
         value: '22-23'
     },
 ];
