3c968b7f51d8051aeac5156b6dff4fa7f94d62d4
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-09-01 18:06:01 -0500

    - added goto today button - added go to week button

--- _components/scheduleKanban/uses/useActionsBar.ts
+++ _components/scheduleKanban/uses/useActionsBar.ts
@@ -4,13 +4,16 @@ import storeFilters from "../store/filters.store";
 import moment from "moment";
 import getTitleFilter from "../actions/getTitleFilter";
 import setUrlParams from "../actions/setUrlParams";
+import storeKanban from "../store/kanban.store";
+import scheduleTypeModel from "../models/scheduleType.model";
 
 export default function useActionsBar(props: any) {
   const proxy = (getCurrentInstance() as any).proxy as any;
   const selectedDate = computed(() => moment(storeFilters.selectedDate));
   const titleFilter = computed(() => storeFilters.titleFilter);
+  const isWeekAgenda = computed(() => storeKanban.scheduleType == scheduleTypeModel[0].value );
   async function changeDate(offset: number): Promise<void> {
-    const dayMultiplier = storeFilters.scheduleType === "week-agenda" ? 7 : 1;
+    const dayMultiplier = storeFilters.scheduleType === scheduleTypeModel[0].value ? 7 : 1;
     const adjustedOffset = dayMultiplier * offset;
 
     storeFilters.selectedDate = selectedDate.value
@@ -32,11 +35,30 @@ export default function useActionsBar(props: any) {
   function openDrawerFilter() {
     storeFilters.showModal = true;
   }
+
+  async function week(){
+    storeKanban.scheduleType = scheduleTypeModel[0].value; //week view
+    await setUrlParams(proxy);
+    await buildKanbanStructure();
+    getTitleFilter()
+  }
+
+  async function today(){
+    storeKanban.scheduleType = scheduleTypeModel[1].value; //day view
+    storeFilters.selectedDate = moment().format('YYYY/MM/DD');
+    await setUrlParams(proxy);
+    await buildKanbanStructure();
+    getTitleFilter()
+  }
+
   return {
     next,
     back,
     openDrawerFilter,
     titleFilter,
+    isWeekAgenda,
+    week,
+    today
   };
 }
 
