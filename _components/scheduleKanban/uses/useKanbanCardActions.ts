a507ceb0ca1066e8257070e9a5f04a3d16b008d5
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-09-07 09:57:13 -0500

    - duplicate fix

--- _components/scheduleKanban/uses/useKanbanCardActions.ts
+++ _components/scheduleKanban/uses/useKanbanCardActions.ts
@@ -127,7 +127,7 @@ export default function useKanbanCardActions(props: any = {}) {
     const newCard = Object.assign({}, col.cards[index])
     delete newCard.id;
     newCard.duplicated = true
-    newCard.editable = true;
+    newCard.editable = false;
     col.cards.splice(index, 0, newCard);
     modalScheduleStore.isEdit = true;
     modalScheduleStore.form = { ...newCard };
