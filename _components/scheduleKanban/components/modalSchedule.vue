712b73bf28ed6a8ba4d96ec7ea1441832470fd5f
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-09-12 11:46:28 -0500

    - fix : modalSchedule validation - removed scheduleForm due it lost the form reference and could not do the validation - added hideInline validation and nullish

--- _components/scheduleKanban/components/modalSchedule.vue
+++ _components/scheduleKanban/components/modalSchedule.vue
@@ -10,18 +10,37 @@
     :width="form.id ? '70%' : '400px'"
     :customClass="`tw-border-l-2 tw-border-${flightStatus ? flightStatus.color : 'gray-100'}`"
   >
-    <scheduleForm />
+    <q-form ref="refFormSchedule">
+      <div
+        class="tw-grid tw-grid-cols-1 tw-gap-4"
+        :class="{'lg:tw-grid-cols-2': form.id && permisionComments}"
+      >
+        <scheduleFields />
+        <div v-if="showCommentsComponent">
+          <comments
+            v-if="form.id && permisionComments"
+            apiRoute="apiRoutes.qramp.comments"
+            :commentableId="Number(form.id)"
+            commentableType="Modules\Ramp\Entities\WorkOrder"
+            permisionComments="ramp.work-orders-comments"
+            class="tw-py-4"
+          />
+        </div>
+      </div>
+    </q-form>
   </master-modal>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import useModalSchedule from '../uses/useModalSchedule'
-import scheduleForm from './scheduleForm.vue'
+import comments from '@imagina/qsite/_components/master/comments/index.vue'
+import scheduleFields from './scheduleFields.vue'
 
 export default defineComponent({
   components: {
-    scheduleForm
+    comments,
+    scheduleFields
   },
   setup (props, {emit}) {
     return {...useModalSchedule(props, emit)}
