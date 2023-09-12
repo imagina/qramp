d34f04a65d32a614bac200ecb574567947c47edc
Wilmerace <wilmerc@imaginacolombia.com>
2023-09-12 16:23:31 -0500

    se mejora y busca la tarjeta en las columnas

--- _components/scheduleKanban/actions/setIndividualCards.ts
+++ _components/scheduleKanban/actions/setIndividualCards.ts
@@ -3,14 +3,14 @@ import showWorkOrders from './showWorkOrders'
 import modalScheduleStore from '../store/modalSchedule.store';
 
 export default async function setIndividualCards(cardId: number): Promise<void> {
-    const column: any = storeKanban.columns.find(item => {
-        return item.date.format('YYYY-MM-DD') === modalScheduleStore.seletedDateColumn
-    });
-    if (!column) return;
     try {
+        const foundCard = storeKanban.columns
+            .flatMap(column => column.cards)
+            .find(card => card.id === cardId);
+
+        if (!foundCard) return;
         const response = await showWorkOrders(cardId);
-        let card = column.cards.find(card => card.id === cardId);
-        card = { ...response.data, editable: false };
+        Object.assign(foundCard, { ...response.data, editable: false });
     } catch (error) {
         console.log(error);
     }
