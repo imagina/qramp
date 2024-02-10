f4ceef1452313dbe607bd927d2c10ecf6c21de03
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-02-10 11:37:12 -0500

    Update offline.js

--- _config/offline.js
+++ _config/offline.js
@@ -1,5 +1,5 @@
 import workOrderList from '../_store/actions/workOrderList.ts';
-import cache from "modules/qsite/_plugins/cache";
+import cache from "@imagina/qsite/_plugins/cache";
 
 
 export default async function (refresh = false) {
