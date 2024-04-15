5beb39fe1806a77eaa3356752f4fec3846ba0cee
Wilmerace <wilmerc@imaginacolombia.com>
2024-04-15 15:21:37 -0500

    +Update: format code

--- _crud/passengers.vue
+++ _crud/passengers.vue
@@ -75,7 +75,7 @@ export default {
       await workOrderList().getCustomerWithContract()
     })
   },
-  beforeDestroy() {
+  beforeUnmount() {
     qRampStore().setFlightList([]);
     qRampStore().setFlightId(null);
   },
