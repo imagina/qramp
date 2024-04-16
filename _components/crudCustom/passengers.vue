27b05eafd96f6747937028fe58452dbf4f48d08a
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-04-16 18:36:50 -0500

    Code is extracted from the passenger and ramp cruds. Now both share the same base file workOrders/index.vue. This is done to solve the refresh issue when saving or updating a record in both cruds.

---
+++ _components/crudCustom/passengers.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <flightDetail />
+  </div>
+</template>
+<script>
+import qRampStore from 'modules/qramp/_store/qRampStore.js'
+import flightDetail from 'modules/qramp/_components/modal/flightDetail.vue';
+import workOrderList from 'modules/qramp/_store/actions/workOrderList.ts'
+
+export default {
+  name: 'RampCrud',
+  emits: ['loading'],
+  components: {
+    flightDetail,
+  },
+  data() {
+    return {
+      crudId: this.$uid(),
+      areaId: null,
+      commentableId: null,
+    }
+  },
+  async created() {
+    this.$nextTick(async () => {
+      await qRampStore().setIsPassenger(true);
+      const previousPath = this.$route.meta.previousPath.split('/')[1];
+      if (!this.$route.path.startsWith(`/${previousPath}`)) {
+          await workOrderList().getAllList();
+      }
+      await workOrderList().getCustomerWithContract()
+    })
+  },
+  computed: {
+    crudData() {
+      return {
+        crudId: this.crudId,
+        entityName: config("main.qfly.entityNames.workOrder"),
+        permission: 'ramp.passenger-work-orders',
+      }
+    },
+    //Crud info
+    crudInfo() {
+      return this.$store.state.qcrudComponent.component[this.crudId] || {}
+    }
+  },
+}
+</script>
+<style lang="scss">
+</style>
