550e7975659727d934d696f48293bf26068a897f
WilmerAce <wilmerc@imaginacolombia.com>
2024-07-22 15:08:48 -0500

    +Update: filter businessUnitId

--- _components/scheduleKanban/actions/getWorkOrderOffline.ts
+++ _components/scheduleKanban/actions/getWorkOrderOffline.ts
@@ -4,19 +4,31 @@ import dataReturnedWorkOrder from '../models/dataReturnedWorkOrder.model'
 import paginateCacheOffline from 'src/plugins/paginateCacheOffline'
 import getFilteredWorkOrdersOffline from './getFilteredWorkOrdersOffline';
 import storeKanban from '../store/kanban.store';
+import qRampStore from 'modules/qramp/_store/qRampStore';
+import {
+  BUSINESS_UNIT_LABOR,
+  BUSINESS_UNIT_PASSENGER,
+  LABOR
+} from "src/modules/qramp/_components/model/constants";
+import { BUSINESS_UNIT_RAMP } from '../../model/constants';
 
 export default async function getWorkOrderOffline(date, page): Promise<WorkOrders> {
     try {
+        const isPassenger = qRampStore().getIsPassenger();
         const workOrdersFiltered = await getFilteredWorkOrdersOffline(date)
+        console.log(workOrdersFiltered)
         storeKanban.statusIdList = workOrdersFiltered.map(workOrder => workOrder.statusId);
-
-        const cardsChunk = await paginateCacheOffline({ data: workOrdersFiltered }, null, page)
+        let businessUnitId: any = isPassenger ? { businessUnitId: BUSINESS_UNIT_PASSENGER } : {businessUnitId: BUSINESS_UNIT_RAMP};
+        if(qRampStore().getTypeWorkOrder() === LABOR) {
+          businessUnitId = { businessUnitId: BUSINESS_UNIT_LABOR }
+        }
+        const cardsChunk = await paginateCacheOffline({ data: workOrdersFiltered }, null, page, 10, {...businessUnitId})
         const data = _.orderBy(
           cardsChunk.data,
           ['scheduleDate'],
           ['asc']
         )
-
+        console.log(data);
         return {
             data,
             meta: {
