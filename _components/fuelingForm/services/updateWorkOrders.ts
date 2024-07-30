f9766013f43b5842531c0bb72b8a6ce2c941a6a5
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-07-30 16:14:17 -0500

    Function to update fueling records in offline mode implemented.

--- _components/fuelingForm/services/updateWorkOrders.ts
+++ _components/fuelingForm/services/updateWorkOrders.ts
@@ -1,17 +1,33 @@
-import baseService from '@imagina/qcrud/_services/baseService.js'
+import baseService from 'src/modules/qcrud/_services/baseService.js'
 import store from '../store'
+import { cacheOffline } from 'src/plugins/utils'
 import qRampStore from 'src/modules/qramp/_store/qRampStore';
+import moment from 'moment-timezone';
 
 export default async function updateWorkOrders() {
+    const API_ROUTE = 'apiRoutes.qramp.workOrders';
+    const payload = await store.payload();
+    const NEW_DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss'
+    const ORIGINAL_DATE_FORMAT = 'MM/DD/YYYY';
+    const payloadOffline = {
+        ...payload,
+        offline: !navigator.onLine
+    }
+
     try {
         store.loading = true;
-        const payload = await store.payload();
         const titleOffline = qRampStore().getTitleOffline();
         const params = {params: {titleOffline}};
-        await baseService.update('apiRoutes.qramp.workOrders', store.form?.id, payload, params)
+        await baseService.update(API_ROUTE, store.form?.id, payload, params)
         store.loading = false;
     } catch (error) {
         console.info(error);
         store.loading = false;
-    }
+    } 
+
+    (payloadOffline as any).scheduleDate = moment(
+        payloadOffline.scheduleDate, 
+        ORIGINAL_DATE_FORMAT
+    ).format(NEW_DATE_FORMAT);
+    await cacheOffline.updateRecord(API_ROUTE, payloadOffline, store.form?.id);
 }
