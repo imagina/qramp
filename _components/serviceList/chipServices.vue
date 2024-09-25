3bbf202de1d0d4a165323b032b699231d1cc03b8
WilmerAce <wilmerc@imaginacolombia.com>
2024-09-25 10:37:08 -0500

    +update: add name employee for api

--- _components/serviceList/chipServices.vue
+++ _components/serviceList/chipServices.vue
@@ -11,17 +11,17 @@ export default defineComponent({
 
 <template>
   <div class="tw-flex tw-items-center">
-    <q-btn   
+    <q-btn
       icon="fa-regular fa-star"
-      :class="`${showFavourite ? 'color-icon-active' : 'color-icon'}`" 
-      size="sm" 
+      :class="`${showFavourite ? 'color-icon-active' : 'color-icon'}`"
+      size="sm"
       round
       flat
       @click="favourites"
       v-if="favouritesList.length > 0 && permissionFavourite.index && !isAppOffline"
     >
-      <q-badge 
-        floating 
+      <q-badge
+        floating
         rounded
         color="primary"
         class="tw-ml-4 tw-px-1 tw--right-0.5"
@@ -29,45 +29,45 @@ export default defineComponent({
         {{ favouritesList.length }}
       </q-badge>
     </q-btn>
-    <hr 
-      v-if="favouritesList.length > 0 && lists.length > 0 && !isAppOffline" 
+    <hr
+      v-if="favouritesList.length > 0 && lists.length > 0 && !isAppOffline"
       class="tw-h-5 tw-w-0.5 tw-mx-2 hr-bg-color"
     />
-    <q-btn   
-      class="color-icon" 
+    <q-btn
+      class="color-icon"
       icon="fa-regular fa-briefcase-blank"
-      size="sm" 
+      size="sm"
       round
       flat
       v-if="lists.length > 0"
     >
-      <q-badge 
-        floating 
+      <q-badge
+        floating
         rounded
         color="primary"
         class="tw-ml-4 tw-px-1 tw--right-1.5"
         v-if="lists.length > 0">
         {{ lists.length }}
       </q-badge>
-      <q-popup-proxy 
-        ref="popupProxyRef"  
+      <q-popup-proxy
+        ref="popupProxyRef"
         content-class="
-          popover-w-490 
-          lg:tw-bg-white 
+          popover-w-490
+          lg:tw-bg-white
           tw-shadow-2xl
           tw-rounded-2xl
-          tw-max-h-72 
+          tw-max-h-72
           lg:tw-overflow-x-hidden
           tw-py-5
           tw-px-2.5"
       >
-        <div 
+        <div
           class="
             popover-w-490
             tw-bg-white
-            lg:tw-max-w-4xl 
-            tw-text-xs 
-            lg:tw-text-base 
+            lg:tw-max-w-4xl
+            tw-text-xs
+            lg:tw-text-base
             tw-max-h-72
             tw-pb-3
             sm:tw-pb-0
@@ -76,64 +76,66 @@ export default defineComponent({
           <div class="text-header-chep tw-mt-5 sm:tw-mt-0 tw-mb-4 tw-ml-3.5">
             Chosen Services
           </div>
-          <div  
-            v-for="(list, index) in lists" 
-            :key="index" 
+          <div
+            v-for="(list, index) in lists"
+            :key="index"
             class="
               tw-grid
               tw-grid-cols-2
               tw-gap-2
               service-syummary
               tw-items-center
-              tw-py-1 
-              hover-color-chep 
-              text-blue-chep 
+              tw-py-1
+              hover-color-chep
+              text-blue-chep
               tw-rounded-full
               text-chep
               tw-pl-3.5
             "
           >
             <section class="tw-flex tw-w-full">
-              <span 
-                @click="searchProduct(list.product_id)" 
+              <span
+                @click="searchProduct(list.product_id)"
                 class="
-                  tw-cursor-pointer 
-                  tw-border-gray-200 
-                  tw-border-r-2 
+                  tw-cursor-pointer
+                  tw-border-gray-200
+                  tw-border-r-2
                   tw-pr-2
-                  tw-mr-2 
+                  tw-mr-2
                   tw-truncate
                 "
               >
                 {{ nameProduct(list.product_id) }}
               </span>
               <div class="tw-flex text-color-bag tw-w-3/5">
-                <div 
+                <div
                   v-for="(work, indeWork) in list.work_order_item_attributes"
                   :key="indeWork"
                   class="tw-flex tw-overflow-hidden"
                 >
                   <span v-if="work.value && indeWork > 0" class="tw-pr-1">,</span>
-                  <span v-if="work.value" class="tw-truncate">{{ work.name }}: 
-                    <span v-if="work.type === 'checkbox'"> 
+                  <span v-if="work.value" class="tw-truncate">{{ work.name }}:
+                    <span v-if="work.type === 'checkbox'">
                       <i class="fa-solid fa-circle-check tw-text-green-500" />
-                    </span> 
-                    <span v-else class="tw-truncate">{{ work.value }}</span>
+                    </span>
+                    <span v-else class="tw-truncate">
+                      {{ work.value }}
+                    </span>
                     <q-tooltip>{{ work.value }}</q-tooltip>
                   </span>
                 </div>
               </div>
             </section>
             <section class="tw-mr-3.5">
-              <i 
+              <i
                 class="
                   fa-light
-                  fa-circle-xmark 
+                  fa-circle-xmark
                   tw-text-xs
                   lg:tw-text-base
-                  color-icon-close 
+                  color-icon-close
                   tw-cursor-pointer
-                " 
+                "
                 @click="deleteChip(list.product_id)"
               />
             </section>
--- _components/serviceList/controllers/chipServices.ts
+++ _components/serviceList/controllers/chipServices.ts
@@ -4,6 +4,7 @@ import findDynamicFieldTitle from '../services/findDynamicFieldTitle';
 import searchAndCreateDynamicField from '../services/searchAndCreateDynamicField';
 import deleteChipRecursive from '../services/deleteChipRecursive';
 import { store } from 'src/plugins/utils'
+import baseService from 'modules/qcrud/_services/baseService.js'
 
 const chipServicesController = (props: any = {}, emit: any = null) => {
     let lists: any = ref([]);
@@ -25,6 +26,20 @@ const chipServicesController = (props: any = {}, emit: any = null) => {
     }
     const updateLists = async () : Promise<void> => {
         lists.value = await serviceListStore().getServiceListSelected();
+        lists.value.forEach(list => {
+          list.work_order_item_attributes.forEach(async item => {
+            if(item.type === 'select' && Array.isArray(JSON.parse(item.value))) {
+              const response = await baseService.index('apiRoutes.qsetupagione.employees', {
+                params: {
+                  filter:{
+                    id: JSON.parse(item.value)
+                  }
+                }
+              })
+              item.value = response.data.map((item) => item.name)
+            }
+          })
+        })
     }
 
     function deleteChip(productId: string): void {
@@ -61,4 +76,4 @@ const chipServicesController = (props: any = {}, emit: any = null) => {
         isAppOffline
     }
 }
-export default chipServicesController;  
+export default chipServicesController;
