45c3e75ac5af7b09451dfc84fc9cbac909bb3a36
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-10-17 11:49:08 -0500

    Remove commented out code for quick cards in main.js

--- _config/main.js
+++ _config/main.js
@@ -14,26 +14,26 @@ export default {
     security: 'security'
   },
   quickCards: [
-    {
-      active: true,
-      permission: 'ramp.dashboard.work-orders-by-status-lines',
-      component: () => import('../_components/quick-cards/lineChart.vue')
-    },
+    // {
+    //   active: true,
+    //   permission: 'ramp.dashboard.work-orders-by-status-lines',
+    //   component: () => import('../_components/quick-cards/lineChart.vue')
+    // },
     {
       active: true,
       permission: 'ramp.billing-closed-date.edit',
       component: () => import('../_components/quick-cards/billingClosedDate')
     },
-    {
-      active: true,
-      permission: 'ramp.dashboard.work-orders-by-status',
-      component: () => import('../_components/quick-cards/bar.vue')
-    },
-    {
-      active: true,
-      permission: 'ramp.dashboard.percentage-work-orders-posted',
-      component: () => import('../_components/quick-cards/percentage.vue')
-    }
+    // {
+    //   active: true,
+    //   permission: 'ramp.dashboard.work-orders-by-status',
+    //   component: () => import('../_components/quick-cards/bar.vue')
+    // },
+    // {
+    //   active: true,
+    //   permission: 'ramp.dashboard.percentage-work-orders-posted',
+    //   component: () => import('../_components/quick-cards/percentage.vue')
+    // }
   ],
   lists: async () => {
     const workOrderList = await import('../_store/actions/workOrderList.ts')
