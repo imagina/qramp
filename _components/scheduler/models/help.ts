8944bcceb098ccfd6cb2f276fe21df7614ad82a9
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2025-02-05 12:22:32 -0500

    Add help functionality to scheduler for dynamic documentation links

---
+++ _components/scheduler/models/help.ts
@@ -0,0 +1,49 @@
+import { cache } from 'src/plugins/utils'
+import qRampStore from '../../../_store/qRampStore'
+import { 
+    BUSINESS_UNIT_RAMP, 
+    BUSINESS_UNIT_PASSENGER, 
+    BUSINESS_UNIT_SECURITY, 
+} from '../../model/constants'
+import { computed } from 'vue'
+
+const BASE_URL = 'https://delightful-ground-0eae6c50f.4.azurestaticapps.net/docs/documentation'
+
+const getToken = async () => {
+    try {
+        const sessionData = await cache.get.item('sessionData')
+        return sessionData.userToken.split(' ')[1]
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const routes = {
+    [BUSINESS_UNIT_RAMP]: 'ramp-module',
+    [BUSINESS_UNIT_PASSENGER]: 'passenger-module',
+    [BUSINESS_UNIT_SECURITY]: 'security-module',
+}
+
+const token = await getToken()
+
+export const help = () => {
+    const path = routes[qRampStore().getBusinessUnitId() || 0]
+
+    const create = computed(() => ({
+        title: 'Create an Work Order',
+        description: `
+            Need help? Check the
+            <a 
+                href='${BASE_URL}/${path}/schedule#create-a-scheduler?token=${token}' 
+                target='_blank'
+                class='tw-text-blue-500'>
+                documentation
+            </a>
+            for more information on creating Scheduler.
+        `
+    }))
+
+    return {
+        create,
+    }
+}
