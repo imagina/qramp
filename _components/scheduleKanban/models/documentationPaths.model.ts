282933abf5422a403fdbfd5042c4eb5ca1653ab4
Sneider Narvaez <sneider.developer@gmail.com>
2025-02-12 02:01:13 -0500

    Add documentation routing model for business units in ScheduleKanban

---
+++ _components/scheduleKanban/models/documentationPaths.model.ts
@@ -0,0 +1,11 @@
+import { 
+    BUSINESS_UNIT_RAMP, 
+    BUSINESS_UNIT_PASSENGER, 
+    BUSINESS_UNIT_SECURITY, 
+} from '../../model/constants'
+
+export const documentationPaths = {
+    [BUSINESS_UNIT_RAMP]: 'ramp-module',
+    [BUSINESS_UNIT_PASSENGER]: 'passenger-module',
+    [BUSINESS_UNIT_SECURITY]: 'security-module',
+}
